9f572d210f96ee773d0a8e33112db0ceb82e4264
Junyoung Kim <junkim327@gmail.com>
2018-08-19 23:38:54 +0900

    Update "Route" pojo

--- app/src/main/java/com/example/junyoung/uiucbus/httpclient/pojos/Route.java
+++ app/src/main/java/com/example/junyoung/uiucbus/httpclient/pojos/Route.java
@@ -5,8 +5,14 @@ import com.google.gson.annotations.SerializedName;
 public class Route {
   @SerializedName("route_color")
   private String routeColor;
+  @SerializedName("route_text_color")
+  private String routeTextColor;
 
   public String getRouteColor() {
     return routeColor;
+  }
+
+  public String getRouteTextColor() {
+    return routeTextColor;
   }
 }
