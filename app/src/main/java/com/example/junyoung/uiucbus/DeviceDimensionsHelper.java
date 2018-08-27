445a98816ba535b555ec560fcd4d35af32ef9c05
Junyoung Kim <junkim327@gmail.com>
2018-08-27 00:08:37 -0500

    Add device dimensions helper methods

---
+++ app/src/main/java/com/example/junyoung/uiucbus/DeviceDimensionsHelper.java
@@ -0,0 +1,21 @@
+package com.example.junyoung.uiucbus;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+
+public class DeviceDimensionsHelper {
+  public static float convertDpToPixel(float dp, Context context){
+    Resources r = context.getResources();
+    return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, r.getDisplayMetrics());
+  }
+
+  // DeviceDimensionsHelper.convertPixelsToDp(25f, context) => (25px converted to dp)
+  public static float convertPixelsToDp(float px, Context context){
+    Resources r = context.getResources();
+    DisplayMetrics metrics = r.getDisplayMetrics();
+    float dp = px / (metrics.densityDpi / 160f);
+    return dp;
+  }
+}
