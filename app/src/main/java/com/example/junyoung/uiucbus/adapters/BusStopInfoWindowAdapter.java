a1e1541ee98d42b6f04dfbab4483affc640e406d
Junyoung Kim <junkim327@gmail.com>
2018-10-07 23:51:40 -0500

    Add BusStopInfoWindowAdapter

---
+++ app/src/main/java/com/example/junyoung/uiucbus/adapters/BusStopInfoWindowAdapter.java
@@ -0,0 +1,30 @@
+package com.example.junyoung.uiucbus.adapters;
+
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.example.junyoung.uiucbus.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.Marker;
+
+public class BusStopInfoWindowAdapter implements GoogleMap.InfoWindowAdapter {
+  private Context mContext;
+
+  public BusStopInfoWindowAdapter(Context context) {
+    mContext = context;
+  }
+
+  @Override
+  public View getInfoWindow(Marker marker) {
+    ImageView windowImage = new ImageView(mContext);
+    windowImage.setImageDrawable(mContext.getDrawable(R.drawable.ic_bus_stop_color));
+
+    return windowImage;
+  }
+
+  @Override
+  public View getInfoContents(Marker marker) {
+    return null;
+  }
+}
